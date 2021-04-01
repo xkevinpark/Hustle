b97d404ce10cb21dd480187a8ee27c71a8fd2433
KP <kp@KPs-MacBook-Pro.local>
2021-04-01 08:51:47 -0700

    Connected to mongodb.

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,23 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+// have environment variables in .env file
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
+// cors middleware 
 app.use(cors());
+// for parsing json
 app.use(express.json());
 
+// database URI
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+// connect to mongo database by passing in uri, flags: {useNewUrlParser: parses mongo strings, useCreateIndex: deal with mongo deprecating index function}
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true,  useUnifiedTopology: true  }
 );
+
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
@@ -24,6 +30,7 @@ const usersRouter = require('./routes/users');
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+// start server listening
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
 });

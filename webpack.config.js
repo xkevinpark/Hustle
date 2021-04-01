07c1eea877cbc7c3ee82cf55b44a6204be737693
KP <kp@KPs-MacBook-Pro.local>
2021-04-01 08:39:14 -0700

    First commit.

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const path = require('path');
+ 
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+    hot: true,
+    port: 8080,
+  },
+};
